10d59089f1e76ad4336efd234a5282c43d96046e
Heksu_Howaito <dev@fakhrif.my.id>
2026-08-03 02:37:42 +0700

    feat(api): update server entry healthcheck endpoint

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,31 @@
 import { Elysia } from "elysia";
+import { db } from "./database/client";
+import { roles } from "./database/schema";
 
-const app = new Elysia().get("/", () => "Hello CivicOS").listen(3000);
+const app = new Elysia()
+  .get("/health", async ({ set }) => {
+    try {
+      const [sampleRole] = await db.select().from(roles).limit(1);
+
+      return {
+        status: "ok",
+        database: "connected",
+        sampleRole,
+        timestamp: new Date().toISOString(),
+      }
+    } catch (error) {
+      set.status = 503;
+      console.error("Health check DB Error", error);
+
+      return {
+        status: "error",
+        database: "disconnected",
+        error: error instanceof Error ? error.message : "Unknown error",
+        timestamp: new Date().toISOString(),
+      };
+    }
+  })
+  .get("/", () => "Hello CivicOS").listen(3000);
 
 console.log(
   `CivicOS is running at ${app.server?.hostname}:${app.server?.port}`
